4b250d0e614477912d0d0880fae3e4b5d1522813
Gianna <gleidich2@gmail.com>
2025-04-11 12:55:49 -0400

    started turning nav bar into multiview ui

---
+++ frontend/src/master.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function navigate(viewID){
+        document.querySelectorAll(".view").forEach((view) => {
+            view.style.display = "none";
+        });
+        document.getElementById(viewID).style.display = "block";
+    }
+
+
+    document
+        .getElementById("home")
+        .addEventListener
+
+    console.log("test");
+
+})
+
+
+
