305b6f2223d00e7b82325ff1d1ab385573021abc
advayav <advaya.venbakkam@gmail.com>
2025-04-25 11:21:47 -0400

    reinstalled express

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import DraggedImageRoutes from '../route/DraggedImageRoutes.js';
-// import WishlistRoutes from '../route/WishlistRoutes.js'; 
-// import cors from 'cors';
+import WishlistRoutes from '../route/WishlistRoutes.js'; 
 
 class Server {
     constructor() {
@@ -15,13 +14,12 @@ class Server {
         this.app.use(express.static("../frontend/"));
         // this.app.use(express.json({ limit: "10mb" }));
         this.app.use(express.json( {limit: "100mb"}));
-        // this.app.use(cors())
     }
    
     setupRoutes() {
         console.log("Setting up routes...");
         this.app.use("/api", DraggedImageRoutes);
-        // this.app.use("/api", WishlistRoutes);
+        this.app.use("/api", WishlistRoutes);
     }
 
     start(port = 3000) {
