3759b67fd7bc7d8c7fd52ae43fc80c75615188b1
Gianna <gleidich2@gmail.com>
2025-05-01 09:32:59 -0400

    redo model factory for new database

--- backend/model/ModelFactory.js
+++ backend/model/ModelFactory.js
@@ -3,25 +3,39 @@ import JSONWishlistModel from "./JSONWishlistModel.js";
 import DraggedImageModel from './DraggedImageModel.js';
 import UserModel from './UserModel.js';
 
+import 
+
 class ModelFactory {
-    static getDraggedImageModel() {
-        return DraggedImageModel;
-    }
-
-    getWishlistModel() {
-        return JSONWishlistModel;
-    }
-
-    static getUserModel() {
-        return UserModel;
+    async getModel(model = "sqlite", type) {
+        if (type === "forDraggedImage") { // draggedImage model 
+            if (model === "sqlite") {
+                return SQLiteDraggedImageModel;
+            } else if (model === "sqlite-fresh") {
+                await SQLiteDraggedImageModel.init(true);
+                return SQLiteDraggedImageModel;
+            } else {
+                return InMemoryDraggedImageModel; // in-memory model
+            }
+        } // else type === YOUR TYPE 
     }
 }
 
-const ModelFactory = new _ModelFactory();
 
+
+// class ModelFactory {
+//     static getDraggedImageModel() {
+//         return DraggedImageModel;
+//     }
+
+//     getWishlistModel() {
+//         return JSONWishlistModel;
+//     }
+
+//     // gave me error down
 //     static getUserModel() {
 //         return UserModel;
 //     }
 // }
 
+const ModelFactory = new ModelFactory();
 export default ModelFactory;
---
+++ backend/model/SQLiteDraggedImageModel.js
@@ -0,0 +1,7 @@
+// import { Sequelize, DataTypes } from "sequelize";
+
+// const sequelize = new Sequelize({
+//     dialect: "sqlite",
+//     storage: "database.sqlite"
+// })
+
